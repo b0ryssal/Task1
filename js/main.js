6124042ad254dd7f7d762d37fcf8ca13371e7a59
Alex Borisevych <santos2003310@gmail.com>
2022-10-26 21:10:45 +0300

    100000

---
+++ js/main.js
@@ -0,0 +1,23 @@
+new Swiper('.swiper', {
+
+    breakpoints: {
+   
+    960: {
+        slidesPerView: 1,
+        
+     
+    },
+ 
+    1200: {
+      slidesPerView: 3,
+     
+    },
+ 
+    1400: {
+      slidesPerView: 4,
+      
+    }
+  }
+   
+    
+});
